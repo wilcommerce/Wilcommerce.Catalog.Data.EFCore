0baf3cfafff62c2441903c5a2790c4841c4b005f
albi.mori <albi.mori@gmail.com>
2017-03-24 14:24:37 +0100

    catalog read models implementation

---
+++ src/Wilcommerce.Catalog.Data.EFCore/ReadModels/CatalogDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Wilcommerce.Catalog.Models;
+using Wilcommerce.Catalog.ReadModels;
+
+namespace Wilcommerce.Catalog.Data.EFCore.ReadModels
+{
+    public class CatalogDatabase : ICatalogDatabase
+    {
+        protected CatalogContext _context;
+
+        public CatalogDatabase(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Brand> Brands => _context.Brands;
+
+        public IQueryable<Category> Categories => _context.Categories;
+
+        public IQueryable<CustomAttribute> CustomAttributes => _context.CustomAttributes;
+
+        public IQueryable<ProductAttribute> ProductAttributes => _context.ProductAttributes;
+
+        public IQueryable<ProductImage> ProductImages => _context.ProductImages;
+
+        public IQueryable<ProductReview> ProductReviews => _context.ProductReviews;
+
+        public IQueryable<Product> Products => _context.Products;
+
+        public IQueryable<CatalogSettings> Settings => _context.CatalogSettings;
+
+        public IQueryable<TierPrice> TierPrices => _context.TierPrices;
+    }
+}
