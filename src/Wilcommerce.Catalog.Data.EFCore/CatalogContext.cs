1ea244631b2550f63eff1d81ff3c2905c600d3c3
albi.mori <albi.mori@gmail.com>
2017-03-24 11:30:53 +0100

    context class

---
+++ src/Wilcommerce.Catalog.Data.EFCore/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wilcommerce.Catalog.Data.EFCore
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
