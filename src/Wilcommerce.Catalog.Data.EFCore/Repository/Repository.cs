31d48024ff614f43b7b7a0fbd68038a8580b86d6
albi.mori <albi.mori@gmail.com>
2017-03-24 16:29:19 +0100

    repository implementation

---
+++ src/Wilcommerce.Catalog.Data.EFCore/Repository/Repository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using Wilcommerce.Catalog.Repository;
+
+namespace Wilcommerce.Catalog.Data.EFCore.Repository
+{
+    public class Repository : IRepository
+    {
+        protected CatalogContext _context;
+
+        public Repository(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public void Add<TModel>(TModel model) where TModel : class, Core.Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                _context.Set<TModel>().Add(model);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public TModel GetByKey<TModel>(Guid key) where TModel : class, Core.Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                return _context.Find<TModel>(key);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<TModel> GetByKeyAsync<TModel>(Guid key) where TModel : class, Core.Infrastructure.IAggregateRoot
+        {
+            try
+            {
+                var model = await _context.FindAsync<TModel>(key);
+                return model;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
