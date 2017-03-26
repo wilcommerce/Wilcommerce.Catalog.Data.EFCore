2f00a5d5da97dac68f3b702db15e7f99baa365e5
albi.mori <albi.mori@gmail.com>
2017-03-26 22:09:22 +0200

    add comments

--- src/Wilcommerce.Catalog.Data.EFCore/ReadModels/CatalogDatabase.cs
+++ src/Wilcommerce.Catalog.Data.EFCore/ReadModels/CatalogDatabase.cs
@@ -4,8 +4,14 @@ using Wilcommerce.Catalog.ReadModels;
 
 namespace Wilcommerce.Catalog.Data.EFCore.ReadModels
 {
+    /// <summary>
+    /// Implementation of <see cref="ICatalogDatabase"/> 
+    /// </summary>
     public class CatalogDatabase : ICatalogDatabase
     {
+        /// <summary>
+        /// The DbContext instance
+        /// </summary>
         protected CatalogContext _context;
 
         public CatalogDatabase(CatalogContext context)
@@ -13,22 +19,49 @@ namespace Wilcommerce.Catalog.Data.EFCore.ReadModels
             _context = context;
         }
 
+        /// <summary>
+        /// Get the list of brands
+        /// </summary>
         public IQueryable<Brand> Brands => _context.Brands;
 
+        /// <summary>
+        /// Get the list of categories
+        /// </summary>
         public IQueryable<Category> Categories => _context.Categories;
 
+        /// <summary>
+        /// Get the list of custom attributes
+        /// </summary>
         public IQueryable<CustomAttribute> CustomAttributes => _context.CustomAttributes;
 
+        /// <summary>
+        /// Get the list of product attributes
+        /// </summary>
         public IQueryable<ProductAttribute> ProductAttributes => _context.ProductAttributes;
 
+        /// <summary>
+        /// Get the list of product images
+        /// </summary>
         public IQueryable<ProductImage> ProductImages => _context.ProductImages;
 
+        /// <summary>
+        /// Get the list of product reviews
+        /// </summary>
         public IQueryable<ProductReview> ProductReviews => _context.ProductReviews;
 
+        /// <summary>
+        /// Get the list of products
+        /// </summary>
         public IQueryable<Product> Products => _context.Products;
 
+        /// <summary>
+        /// Get the list of catalog settings
+        /// </summary>
         public IQueryable<CatalogSettings> Settings => _context.CatalogSettings;
 
+        /// <summary>
+        /// Get the list of tier prices
+        /// </summary>
         public IQueryable<TierPrice> TierPrices => _context.TierPrices;
     }
 }
--- src/Wilcommerce.Catalog.Data.EFCore/Repository/Repository.cs
+++ src/Wilcommerce.Catalog.Data.EFCore/Repository/Repository.cs
@@ -4,8 +4,14 @@ using Wilcommerce.Catalog.Repository;
 
 namespace Wilcommerce.Catalog.Data.EFCore.Repository
 {
+    /// <summary>
+    /// Implementation of <see cref="IRepository"/> 
+    /// </summary>
     public class Repository : IRepository
     {
+        /// <summary>
+        /// The DbContext instance
+        /// </summary>
         protected CatalogContext _context;
 
         public Repository(CatalogContext context)
@@ -13,6 +19,9 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             _context = context;
         }
 
+        /// <summary>
+        /// Dispose all the resource used
+        /// </summary>
         public void Dispose()
         {
             if (_context != null)
@@ -23,6 +32,9 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Saves all the changes made on the aggregate
+        /// </summary>
         public void SaveChanges()
         {
             try
@@ -35,6 +47,10 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             }
         }
 
+        /// <summary>
+        /// Async method. Saves all the changes made on the aggregate
+        /// </summary>
+        /// <returns></returns>
         public async Task SaveChangesAsync()
         {
             try
@@ -47,6 +63,11 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             }
         }
 
+        /// <summary>
+        /// Add an aggregate to the repository
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="model">The aggregate to add</param>
         public void Add<TModel>(TModel model) where TModel : class, Core.Infrastructure.IAggregateRoot
         {
             try
@@ -59,6 +80,12 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             }
         }
 
+        /// <summary>
+        /// Gets the aggregate based on the specified key
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="key">The key of the aggregate to search</param>
+        /// <returns>The aggregate found</returns>
         public TModel GetByKey<TModel>(Guid key) where TModel : class, Core.Infrastructure.IAggregateRoot
         {
             try
@@ -71,6 +98,12 @@ namespace Wilcommerce.Catalog.Data.EFCore.Repository
             }
         }
 
+        /// <summary>
+        /// Async method. Gets the aggregate based on the specified key
+        /// </summary>
+        /// <typeparam name="TModel">The aggregate's type</typeparam>
+        /// <param name="key">The key of the aggregate to search</param>
+        /// <returns>The aggregate found</returns>
         public async Task<TModel> GetByKeyAsync<TModel>(Guid key) where TModel : class, Core.Infrastructure.IAggregateRoot
         {
             try
