9b9380bb44cbe7131069083663bb0bc74d3dc500
albi.mori <albi.mori@gmail.com>
2017-11-21 13:48:20 +0100

    setup test project

---
+++ Wilcommerce.Catalog.Data.EFCore.Test/Fixtures/CatalogContextFixture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Wilcommerce.Catalog.Data.EFCore.Test.Fixtures
+{
+    public class CatalogContextFixture : IDisposable
+    {
+        public CatalogContext Context { get; protected set; }
+
+        public CatalogContextFixture()
+        {
+            BuildContext();
+        }
+
+        public void Dispose()
+        {
+            CleanData();
+            if (Context != null)
+            {
+                Context.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void PrepareData()
+        {
+
+        }
+
+        protected virtual void CleanData()
+        {
+
+        }
+
+        protected virtual void BuildContext()
+        {
+            var options = new DbContextOptionsBuilder<CatalogContext>()
+                .UseInMemoryDatabase(databaseName: "InMemory-Catalog")
+                .Options;
+
+            Context = new CatalogContext(options);
+        }
+    }
+}
